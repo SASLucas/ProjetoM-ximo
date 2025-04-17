e5b4b008122cc2ccae226a2947bb680881fbf642
SASLucas <lsas3@aluno.ifal.edu.br>
2025-04-17 14:34:06 -0300

    Update monitor.js

--- Projeto terceiro bimeste/modelo/monitor.js
+++ Projeto terceiro bimeste/modelo/monitor.js
@@ -1,20 +1,10 @@
 const Aluno = require('../modelo/aluno');
 
 class Monitor extends Aluno {
-    constructor(id, nome, email, disciplinaMonitoria) {
-        super(id, nome, email);
+    constructor(id, nome, disciplinaMonitoria) {
+        super(id, nome);
         this.disciplina = disciplinaMonitoria;
         this.plantoes = [];
-    }
-
-    marcarPlantao(data, horario) {
-        this.plantoes.push({ data, horario });
-        console.log(`Plantão marcado para ${data} às ${horario}`);
-    }
-
-    registrarAtividade(atividade) {
-        console.log(`${this.nome} registrou a atividade: ${atividade}`);
-    }
 }
 
 module.exports = Monitor;
