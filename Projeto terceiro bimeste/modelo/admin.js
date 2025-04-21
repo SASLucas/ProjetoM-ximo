b015cc8b5ec4c0e8d5e53527f3e2908e685505ba
LostFromFate11 <vgpn1@aluno.ifal.edu.br>
2025-04-20 22:29:25 -0300

    Add files via upload

--- Projeto terceiro bimeste/modelo/admin.js
+++ Projeto terceiro bimeste/modelo/admin.js
@@ -4,13 +4,39 @@ class Admin{
     constructor(user){
         this.user = user
     }
-    criarMonitoria(disciplina){
-        const novaMonitoria = new Monitoria(disciplina);
-        return novaMonitoria
+//     criarMonitoria(disciplina){
+//         const novaMonitoria = new Monitoria(disciplina);
+//         return novaMonitoria
+//     }
+//     cadastrarProfessor(nome, monitoria){
+//         monitoria.professor = nome
+//     }
+    criarMonitoria(con,disciplina,user) {
+        const criar_monitoria = 'INSERT INTO monitorias (disciplina,user) VALUES (?, ?);'
+        
+        con.query(criar_monitoria, [disciplina,user], (err, results) => {
+          if (err) {
+            console.error('Erro ao criar monitoria:', err);
+          } else {
+            console.log('Monitoria de ${disciplina} criada e vinculada ao professor ${user}');
+       }
+    });
     }
-    cadastrarProfessor(nome, monitoria){
-        monitoria.professor = nome
-    }
-}
+    
+    AdiconarProfessor(con,user,idProfessoe){
+    cosnt adicionar_prof = 'INSERT INTO profesoor(user,idProfessoe)VALUES(?,?)'
+
+    con.query(adicionar_prof,[user,idProfessoe],((err, results) => {
+              if (err) {
+                console.error('Erro ao adicioanr professor:', err);
+              } else {
+                console.log('user ${iuser} adicionado com sucesso');
+              }
+            });
+          }
+        }
+
+ }
+    
 
 module.exports = Admin;
--- Projeto terceiro bimeste/modelo/professor.js
+++ Projeto terceiro bimeste/modelo/professor.js
@@ -6,13 +6,94 @@ class Professor{
         this.nome = nome,
         this.disciplina = disciplina
     }
-    cadastrarMonitor(monitoria, novoMonitor){
-        monitoria.monitor = novoMonitor
-    }
-    cadastrarAluno(monitoria, alunoNovo){
-        monitoria.aluno.push(alunoNovo)
+    // cadastrarMonitor(monitoria, novoMonitor){
+    //     monitoria.monitor = novoMonitor
+    // }
+    // cadastrarAluno(monitoria, alunoNovo){
+    //     monitoria.aluno.push(alunoNovo)
+    // }
+
+    
+    cadastrarAtividade(con,conteudo,idatividade) {
+        const query = 'INSERT INTO atividades (conteudo,idatividade) VALUES (?, ?);'
+
+        con.query(query, [conteudo,idatividade], (err, results) => {
+          if (err) {
+            console.error('Erro ao cadastrar atividade:', err);
+          } else {
+            console.log('atividae cadastrada com sucesso', results);
+       }
+    });
     }
 
+    
+    excluirAtividade(con,idatividade) {
+        const excluir_atividade = 'DELETE FROM atividade where idatividade = ?'
+
+        con.query(excluir_atividade, [idatividade], (err, results) => {
+          if (err) {
+            console.error('Erro ao excluir a atividade:', err);
+          } else {
+            console.log('atividade excluida com sucessor', results);
+       }
+    });
+    }
+
+    
+    cadastrarAluno(con,user) {
+        const adicionar_aluno = 'INSERT INTO aluno (user) VALUES (?);'
+
+        con.query(adicionar_aluno, [user], (err, results) => {
+          if (err) {
+            console.error('Erro ao cadastrar aluno:', err);
+          } else {
+            console.log('aluno cadastrado com sucesso', results);
+       }
+    });
+    }
+
+    
+    excluirAluno(con,user) {
+        const excluir_aluno = 'delete from aluno where user = ?;'
+
+        con.query(excluir_aluno, [user], (err, results) => {
+          if (err) {
+            console.error('Erro ao excluir aluno:', err);
+          } else {
+            console.log('aluno excluido com sucesso', results);
+       }
+    });
+    }
+
+
+    analisarRelatorio(con,idRelatorio){
+const analisar_Relatorio = 'select relatorio from monitor where idRelatorio = ?'
+        con.query(analisar_Relatorio,[idRelatorio], (err, results) => {
+              if (err) {
+                console.error('Erro ao acessar relatorio:', err);
+              } else {
+                console.log('relatorio acessado com sucesso', results);
+           }
+        });
+    }
+
+     verFedeback(con,idFedeback){
+    const ver_fedeback = 'select fedeback from monitor where idFedeback = ?'
+            con.query(ver_fedeback,[idFedeback], (err, results) => {
+                  if (err) {
+                    console.error('Erro ao ver fedeback:', err);
+                  } else {
+                    console.log('fedeback acessado com sucesso', results);
+               }
+            });
+        }
+
+
+    
+    
 }
+
+
+
 
 module.exports = Professor;
